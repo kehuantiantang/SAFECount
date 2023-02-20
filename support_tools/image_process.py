e9eccefa68ee04909b8de074c76c53c8af390b30
jbnu <sober@jbnu.ac.kr>
2023-02-21 03:47:05 +0900

    find area of pipeline

--- support_tools/image_process.py
+++ support_tools/image_process.py
@@ -7,9 +7,9 @@
 import json
 import multiprocessing
 from multiprocessing.pool import ThreadPool
-
-from tqdm import tqdm
-
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 from data.Chicken.gen_gt_density import points2density, apply_scoremap
 import os.path as osp
 import os
@@ -58,6 +58,78 @@ def save2file(target_path, filename, img, points, density, vis_heatmap = False):
         cv2.imwrite(os.path.join(gt_density_map, name + '_vis.jpg'), mask)
 
 
+def find_line(path):
+    img = cv2.imread(path)
+    height, width, _ = img.shape
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_blur = cv2.GaussianBlur(gray, (3, 3), 0)
+
+    kernel = np.ones((9, 9), np.uint8)
+    erode = cv2.erode(img_blur, kernel, iterations=1)
+
+
+    ret, thresh = cv2.threshold(erode, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    thresh = 255 - thresh
+
+    close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel=(3, 3), iterations=10)
+
+    minLineLength = 2500
+    maxLineGap = 800
+    lines = cv2.HoughLinesP(close, 1, np.pi / 180, 100, minLineLength, maxLineGap)
+
+    rm_line = img.copy()
+    pipe_raw = np.zeros_like(img, dtype=np.uint8)
+    pipe_blur = np.zeros((height, width), np.uint8)
+
+    for line in lines[:, 0, :]:
+        x1, y1, x2, y2 = line
+        # if abs(y1 - y2) ^ 2 + abs(x1 - x2) ^ 2 > 0:
+        #     rm_line = cv2.line(rm_line, (x1, y1), (x2, y2), (255, 0, 255), 5)
+        rm_line = cv2.line(rm_line, (0, y1), (width, y2), (255, 0, 255), 5)
+
+        pipe_raw[y1-200:y2+200, 0:width] = img[y1-200:y2+200, 0:width]
+        pipe_blur[y1 - 200:y2 + 200, 0:width] = img_blur[y1 - 200:y2 + 200, 0:width]
+
+
+
+
+    # show_image(sobelx, 'sobelx')
+    # show_image(sobelxy, 'sobelxy')
+    # show_image(edges)
+    # show_image(thresh)
+    # show_image(close, 'close')
+    show_image(rm_line, 'line', cmap=None)
+    show_image(pipe_raw, 'crop', cmap=None)
+
+    return pipe_raw, pipe_blur
+
+# def find_line(path):
+#     img = cv2.imread(path)
+#     height, width, _ = img.shape
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     img_blur = cv2.GaussianBlur(gray, (3, 3), 0)
+#
+#     fx, fy = 4137.8, 4147.3
+#     K = np.array([[fx, 0, height//2], [0, fy, width//2], [0, 0, 1]])
+#     dist_coeff = np.array([k1, k2, p1, p2, k3])
+#
+#     img_shape = gray.shape[::-1]
+#     new_K, roi = cv2.getOptimalNewCameraMatrix(K, dist_coeff, img_shape, 1, img_shape)
+#
+#     dst = cv2.undistort(img, K, dist_coeff, None, new_K)
+#     show_image(dst, 'dst', cmap=None)
+
+def find_contours(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.medianBlur(gray, 5)
+    ret, thresh = cv2.threshold(blur, 200, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        if len(cnt) > 100:
+            ellipse = cv2.fitEllipse(cnt)
+            cv2.ellipse(img, ellipse, (0, 255, 0), 2)
+    show_image(thresh)
+    show_image(img, 'img', cmap=None)
 
 def operation(root, filename):
     img = cv2.imread(osp.join(root, filename))
@@ -114,29 +186,34 @@ def operation(root, filename):
 
 if __name__ == '__main__':
     # ostd method
-    import numpy as np
-    import cv2
-    from matplotlib import pyplot as plt
-    source_path, target_path = '/Volumes/datasets/chicken_count', '/Volumes/datasets/chicken_count_process'
 
+    # source_path, target_path = '/Volumes/datasets/chicken_count', '/Volumes/datasets/chicken_count_process'
+    #
+    #
+    # files = []
+    # for root, _, filenames in os.walk(source_path):
+    #     for filename in filenames:
+    #         if filename.endswith('jpg'):
+    #             files.append((root, filename))
+    #
+    # bar = tqdm(total=len(files), desc='total')
+    # update = lambda *args: bar.update()
+    #
+    # pool = ThreadPool(10)
+    #
+    #
+    # for root, filename in sorted(files):
+    #     kwds = {'root':root, 'filename':filename}
+    #     # video2frame(**kwds)
+    #     pool.apply_async(operation, kwds=kwds, callback=update)
+    #
+    # pool.close()
+    # pool.join()
 
-    files = []
-    for root, _, filenames in os.walk(source_path):
-        for filename in filenames:
-            if filename.endswith('jpg'):
-                files.append((root, filename))
+    pipe_line, pipe_blur = find_line(
+        '/Users/sober/Downloads/0_8_IPC1_20220905000000/input/0_8_IPC1_20220905000000_00000000'
+                             '.jpg')
 
-    bar = tqdm(total=len(files), desc='total')
-    update = lambda *args: bar.update()
+    find_contours(pipe_line)
 
-    pool = ThreadPool(10)
-
-
-    for root, filename in sorted(files):
-        kwds = {'root':root, 'filename':filename}
-        # video2frame(**kwds)
-        pool.apply_async(operation, kwds=kwds, callback=update)
-
-    pool.close()
-    pool.join()
 
